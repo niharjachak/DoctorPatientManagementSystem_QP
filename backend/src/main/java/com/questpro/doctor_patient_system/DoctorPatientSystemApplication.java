c5a0d18f567c6054d1b78d882ee800ecd33d3240
Nihar <nihar.jachak@gmail.com>
2026-03-16 18:37:03 +0530

    Initial Commit: Added Backend SpringBoot project

---
+++ backend/src/main/java/com/questpro/doctor_patient_system/DoctorPatientSystemApplication.java
@@ -0,0 +1,13 @@
+package com.questpro.doctor_patient_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPatientSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPatientSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/questpro/doctor_patient_system/DoctorPatientSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questpro.doctor_patient_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorPatientSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
